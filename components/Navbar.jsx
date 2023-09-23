5d5f61656dee0872e291c15769c08f2516c983d9
Mona-Aboobida-Ahmad <108257936+Mona-Aboobida-Ahmad@users.noreply.github.com>
2023-09-23 10:51:53 -0700

    Add files via upload

---
+++ components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+  
+
+const Navbar = () => {
+  return (
+    <div className='fiexd w-full h-[80px]flex justify-between items-center px-4 bg-[#0a192f]'> jsxpage</div>
+  )
+}
+
+export default Navbar
